dad00e854b7ef3325de55886b0d435c5c07bbcdf
Pavel <pashakovalenko@yandex.ru>
2014-10-15 12:43:44 +0400

    Now del nearly works

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ struct Data {
 };
 
 struct BtreeNode {
-    char islist;
+    int islist;
     int n, maxn;
     int offset;
     struct Key key[2 * LIST_SIZE];
@@ -101,7 +101,7 @@ struct BtreeNode *readNode(int offset, struct DB *db) {
 
 void writeNode(struct BtreeNode *node, struct DB *db) {
     fseek(db->dbfl, node->offset * PAGE_SIZE, SEEK_SET);
-    fwrite(node, PAGE_SIZE, 1, db->dbfl);
+    fwrite(node, PAGE_SIZE - 1, 1, db->dbfl);
     return;
 }
 
@@ -110,6 +110,10 @@ struct BtreeNode *newNode(struct DB *db) {
     res->offset = db->froff;
     db->froff++;
     return res;
+}
+
+void freeNode(struct BtreeNode *node, struct DB *db) {
+    return;
 }
 
 int splitChild(struct BtreeNode *node, int pos, struct DB *db) {
@@ -123,7 +127,7 @@ int splitChild(struct BtreeNode *node, int pos, struct DB *db) {
     for (int i = 0; i < nch->n; i++) {
         nch->key[i] = och->key[i + size];
         nch->data[i] = och->data[i + size];
-        nch->link[i] = nch->link[i + size];
+        nch->link[i] = och->link[i + size];
     }
     nch->link[nch->n] = och->link[och->n];
     node->link[node->n + 1] = node->link[node->n];
@@ -201,7 +205,8 @@ int put(struct BtreeNode *node, struct Key *key, struct Data *data, struct DB *d
     }
 }
 
-int put_inter(struct Key *key, struct Data *data, struct DB *db) {
+int put_inter(struct Key *key, struct Data *data, struct DB *db)
+{
     struct BtreeNode *root = db->root;
     if (root->n >= 2 * root->maxn - 1) {
         struct BtreeNode *oroot = root;
@@ -226,12 +231,11 @@ void stk(struct Key *a, char *b)
     return;
 }
 
-void findmax(struct BtreeNode *node, struct Key *key, struct Data *data, struct DB *db) {
+void findmax(struct BtreeNode *node, struct Key *key, struct Data *data, struct DB *db)
+{
     if (node->islist) {
         *key = node->key[node->n - 1];
         *data = node->data[node->n - 1];
-        node->n--;
-        writeNode(node, db);
         return;
     } else {
         struct BtreeNode *ch = readNode(node->link[node->n], db);
@@ -241,13 +245,11 @@ void findmax(struct BtreeNode *node, struct Key *key, struct Data *data, struct 
     }
 }
 
-void findmin(struct BtreeNode *node, struct Key *key, struct Data *data, struct DB *db) {
+void findmin(struct BtreeNode *node, struct Key *key, struct Data *data, struct DB *db)
+{
     if (node->islist) {
         *key = node->key[0];
         *data = node->data[0];
-        for (int i = 0; i < node->n - 1)
-        node->n--;
-        writeNode(node, db);
         return;
     } else {
         struct BtreeNode *ch = readNode(node->link[0], db);
@@ -259,31 +261,239 @@ void findmin(struct BtreeNode *node, struct Key *key, struct Data *data, struct 
 
 int del(struct BtreeNode *node, struct Key *key, struct DB *db)
 {
+    /*printf("\n\n\n");
+    printTree(db->root, db, 2);*/
     int i = 0;
     while ((i < node->n) && (keycmp(&(node->key[i]), key) < 0)) {
         i++;
     }
-    if (keycmp(&(node->key[i]), key) == 0) {
+    if ((i < node->n) && (keycmp(&(node->key[i]), key) == 0)) {
         if (node->islist) {
-            for (int j = i; j < node->n; j++) {
+            for (int j = i; j < node->n - 1; j++) {
                 node->key[j] = node->key[j + 1];
                 node->data[j] = node->data[j + 1];
             }
             node->n--;
+            writeNode(node, db);
             return 0;
         } else {
             struct BtreeNode *left = readNode(node->link[i], db);
             if (left->n >= left->maxn) {
                 findmax(left, &node->key[i], &node->data[i], db);
+                del(left, &node->key[i], db);
+                writeNode(node, db);
                 free(left);
                 return 0;
             }
             struct BtreeNode *right = readNode(node->link[i + 1], db);
             if (right->n >= right->maxn) {
+                findmin(right, &node->key[i], &node->data[i], db);
+                del(right, &node->key[i], db);
+                writeNode(node, db);
+                free(left);
+                free(right);
+                return 0;
+            }
+            int s1 = left->n;
+            left->key[s1] = node->key[i];
+            left->data[s1] = node->data[i];
+            for (int j = i; j < node->n - 1; j++) {
+                node->key[j] = node->key[j + 1];
+                node->data[j] = node->data[j + 1];
+                node->link[j + 1] = node->link[j + 2];
+            }
+            node->n--;
+            for (int j = 0; j < right->n; j++) {
+                left->key[s1 + j + 1] = right->key[j];
+                left->data[s1 + j + 1] = right->data[j];
+                left->link[s1 + j + 1] = right->link[j];
+            }
+            left->n = s1 + right->n + 1;
+            left->link[left->n] = right->link[right->n];
+            freeNode(right, db);
+            writeNode(node, db);
+            int res = del(left, key, db);
+            free(left);
+            return res;
+        }
+    } else {
+        struct BtreeNode *ch = readNode(node->link[i], db);
+        if (ch->n < ch->maxn) {
+            struct BtreeNode *left, *right;
+            if (i > 0) {
+                left = readNode(node->link[i - 1], db);
+                if (left->n >= left->maxn) {
+                    //Taking one child from the left neighbour
+                    ch->link[ch->n + 1] = ch->link[ch->n];
+                    for (int j = ch->n - 1; j >= 0; j--) {
+                        ch->key[j + 1] = ch->key[j];
+                        ch->data[j + 1] = ch->data[j];
+                        ch->link[j + 1] = ch->link[j];
+                    }
+                    ch->n++;
+                    ch->key[0] = node->key[i - 1];
+                    ch->data[0] = node->data[i - 1];
+                    node->key[i - 1] = left->key[left->n - 1];
+                    node->data[i - 1] = left->data[left->n - 1];
+                    ch->link[0] = left->link[left->n];
+                    left->n--;
+                    writeNode(left, db);
+                    free(left);
+                    writeNode(node, db);
+                    int res = del(ch, key, db);
+                    free(ch);
+                    return res;
+                }
+            }
+            if (i < node->n) {
+                right = readNode(node->link[i + 1], db);
+                if (right->n >= right->maxn) {
+                    //Taking one child from the right neighbour
+                    ch->key[ch->n] = node->key[i];
+                    ch->data[ch->n] = node->data[i];
+                    ch->link[ch->n + 1] = right->link[0];
+                    ch->n++;
+                    node->key[i] = right->key[0];
+                    node->data[i] = right->data[0];
 
+                    for (int j = 0; j < right->n - 1; j++) {
+                        right->key[j] = right->key[j + 1];
+                        right->data[j] = right->data[j + 1];
+                        right->link[j] = right->link[j + 1];
+                    }
+                    right->link[right->n - 1] = right->link[right->n];
+                    right->n--;
+
+                    free(left);
+                    writeNode(right, db);
+                    free(right);
+                    writeNode(node, db);
+                    int res = del(ch, key, db);
+                    free(ch);
+                    return res;
+                }
+            }
+            if (i > 0) {
+                //Merging with left neighbour
+                int size = left->n;
+                left->key[size] = node->key[i - 1];
+                left->data[size] = node->data[i - 1];
+                for (int j = 0; j < ch->n; j++) {
+                    left->key[size + j + 1] = ch->key[j];
+                    left->data[size + j + 1] = ch->data[j];
+                    left->link[size + j + 1] = ch->link[j];
+                }
+                left->n = size + ch->n + 1;
+                left->link[left->n] = ch->link[ch->n];
+                freeNode(ch, db);
+                free(ch);
+                for(int j = i - 1; j < node->n - 1; j++) {
+                    node->key[j] = node->key[j + 1];
+                    node->data[j] = node->data[j + 1];
+                    node->link[j + 1] = node->link[j + 2];
+                }
+                //node->link[node->n - 1] = node->link[node->n];
+                node->n--;
+                writeNode(node, db);
+                free(right);
+                writeNode(left, db);
+                int res = del(left, key, db);
+                free(left);
+                return res;
+            }
+            if (i < node->n) {
+                //Merging with right neighbour
+                int size = ch->n;
+                ch->key[size] = node->key[i];
+                ch->data[size] = node->data[i];
+                for (int j = 0; j < right->n; j++) {
+                    ch->key[size + j + 1] = right->key[j];
+                    ch->data[size + j + 1] = right->data[j];
+                    ch->link[size + j + 1] = right->link[j];
+                }
+                ch->n = size + right->n + 1;
+                ch->link[ch->n] = right->link[right->n];
+                freeNode(right, db);
+                free(right);
+                for(int j = i; j < node->n - 1; j++) {
+                    node->key[j] = node->key[j + 1];
+                    node->data[j] = node->data[j + 1];
+                    node->link[j + 1] = node->link[j + 2];
+                }
+                //node->link[node->n - 1] = node->link[node->n];
+                node->n--;
+                writeNode(node, db);
+                free(left);
+                writeNode(ch, db);
+                int res = del(ch, key, db);
+                free(ch);
+                return res;
+            }
+            return -1;
+        } else {
+            if (!node->islist) {
+                int res = del(ch, key, db);
+                free(ch);
+                return res;
+            } else {
+                return -1;
             }
         }
     }
+}
+
+void printTree(struct BtreeNode *node, struct DB *db, int k)
+{
+    struct BtreeNode *ch;
+    for (int i = 0; i < node->n; i++) {
+        if (!node->islist) {
+            ch = readNode(node->link[i], db);
+            printTree(ch, db, k + 1);
+            free(ch);
+        }
+        for(int j = 0; j < k; j++) printf("--");
+        printf("{%s : %s}\n", node->key[i].data, node->data[i].data);
+    }
+    if (!node->islist) {
+        ch = readNode(node->link[node->n], db);
+        printTree(ch, db, k + 1);
+        free(ch);
+    }
+    return;
+}
+
+int del_inter(struct Key *key, struct DB *db)
+{
+    int res = del(db->root, key, db);
+    if (db->root->n == 0) {
+        struct BtreeNode *temp = readNode(db->root->link[0], db);
+        freeNode(db->root, db);
+        free(db->root);
+        db->root = temp;
+    }
+    return res;
+}
+
+void init(struct Key *mykey, struct Data *mydata) {
+    stk(&mykey[0], "0");
+    stk(&mydata[0], "My Little Pony");
+    stk(&mykey[1], "1");
+    stk(&mydata[1], "Fire brigade");
+    stk(&mykey[2], "20");
+    stk(&mydata[2], "Trick and Treat");
+    stk(&mykey[3], "3");
+    stk(&mydata[3], "\\(``)/");
+    stk(&mykey[4], "4");
+    stk(&mydata[4], "For the glory");
+    stk(&mykey[5], "5");
+    stk(&mydata[5], "Friendship is magic");
+    stk(&mykey[6], "6");
+    stk(&mydata[6], "Monty Python");
+    stk(&mykey[7], "7");
+    stk(&mydata[7], "Griffindor");
+    stk(&mykey[8], "8");
+    stk(&mydata[8], "Conquest");
+    return;
 }
 
 int main()
@@ -292,38 +502,39 @@ int main()
     sett.db_size = 400096;
     struct DB *mydb = dbcreate("db.txt", sett);
 
-    int size = 300, k = 8;
+    int size = 300, k = 9;
     struct Key mykey[size];
     struct Data mydata[size];
     //memset(mydata, 0, sizeof(mydata));
     //memset(mykey, 0, sizeof(mykey));
 
-    stk(&mykey[0], "rtyhv");
-    stk(&mydata[0], "My Little Pony");
-    stk(&mykey[1], "ftgvbjnhugf5");
-    stk(&mydata[1], "Fire brigade");
-    stk(&mykey[2], "8bvf67un  d");
-    stk(&mydata[2], "Trick and Treat");
-    stk(&mykey[3], "h v567tgv ");
-    stk(&mydata[3], "\\(``)/");
-    stk(&mykey[4], " b y677ft");
-    stk(&mydata[4], "For the glory");
-    stk(&mykey[5], "jbuvgyc6578ui");
-    stk(&mydata[5], "Friendship is magic");
-    stk(&mykey[6], " hug767gyb");
-    stk(&mydata[6], "Monty Python");
-    stk(&mykey[7], "9087tyvgh b");
-    stk(&mydata[7], "Griffindor");
+    init(&mykey, &mydata);
 
     for (int i = 0; i < k; i++)
         put_inter(&mykey[i], &mydata[i], mydb);
+    printTree(mydb->root, mydb, 1);
+
+
+    for (int i = 0; i < k - 1; i++) {
+    printf("%d\n", del_inter(&mykey[i], mydb));
+    printf("\n\n\n\n");
+    printTree(mydb->root, mydb, 1);
+    }
+    /*
+    printf("%d\n", del(mydb->root, &mykey[2], mydb));
+    printf("\n\n\n\n");
+    printTree(mydb->root, mydb, 1);
+    */
+
+    /*printf("\n\n\n\n");
     for (int i = 0; i < k; i++) {
-        get(mydb->root, &mykey[i], &mydata[0], mydb);
+        printf("%d\n", get(mydb->root, &mykey[i], &mydata[0], mydb));
         puts(mydata[0].data);
         //printf("%d %d%d%d%d%\n", mydata[0].size, mydata[0].data[0], mydata[0].data[1], mydata[0].data[2], mydata[0].data[3]);
     }
 
     printf("%d %d\n", (int) sizeof(struct BtreeNode), LIST_SIZE);
+    */
     //scanf("\n");
     return 0;
 }
